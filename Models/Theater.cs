8f0db81040124399ecd04b48aed36581e9e52211
JevonSebastian <jevonsebastian952@gmail.com>
2023-12-01 11:59:21 +0700

    feat: added models, database config in appsettings.json and Program.cs

---
+++ Models/Theater.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GalaxyCinemaBackEnd.Models
+{
+    public class Theater
+    {
+        [Key]
+        public int TheaterID { get; set; }
+        [Required]
+        public String Name { get; set; }
+        [Required]
+        public String Address { get; set; }
+      
+    }
+}
